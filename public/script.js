72591c99539f7f07d1960c5f41c20e629d4aebb1
Fernando204 <151165080+Fernando204@users.noreply.github.com>
2024-07-10 15:41:30 -0300

    criação do chat

---
+++ public/script.js
@@ -0,0 +1,28 @@
+const socket = io()
+const form = document.getElementById('formulario')
+const logi = document.getElementById('login')
+const cvs = document.getElementById('conversa')
+
+const logArea = document.querySelector('.Login')
+const chatArea = document.querySelector('.chat')
+
+var username = '';
+
+logi.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    logArea.style.display = 'none'
+    chatArea.style.display = 'block'
+    username = document.getElementById('name').value
+    socket.emit('command', username)
+})
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    var inpt = document.getElementById('input').value 
+    socket.emit('chat message', `${username}: ${inpt}`)
+})
+socket.on('chat message', (msg) => {
+    var item = document.createElement('li')
+    item.textContent = msg
+    cvs.appendChild(item)
+})
---
+++ zap.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
+
+var quantidade_de_mensagens = 0;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection', (socket) => {
+
+    console.log('alguem entrou no chat')
+
+    socket.on('command', (msg)=>{
+        console.log(`usuario: ${msg} entrou no chat`)
+    })
+
+    socket.on('chat message', (msg) => {
+        quantidade_de_mensagens++
+        console.log(`quantide de mensagens: ${quantidade_de_mensagens}`)
+        io.emit('chat message', msg)
+    })
+})
+
+
+server.listen(5001, () => {
+    console.log('server ligado na url: http://localhost:5001')
+})
+
